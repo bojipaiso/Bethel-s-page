fd9d6b94abb7fc49c570f9fd1ac30a784e7c9c7e
dev-pink <pinky_villanueva@bethel.edu.ph>
2026-04-05 00:41:34 +0800

    crud completion!!

--- reference/includes/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-// includes/auth.php
-session_start();
-
-if(!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
-    header("Location: login.php");
-    exit();
-}
-?>
